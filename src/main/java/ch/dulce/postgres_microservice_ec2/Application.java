64e835eab3d1cc1a0bc49dc57826c2c613278a39
Antonio Dolce <dolce.antonio@gmail.com>
2024-08-04 19:18:02 +0200

    Initial commit

---
+++ src/main/java/ch/dulce/postgres_microservice_ec2/Application.java
@@ -0,0 +1,13 @@
+package ch.dulce.postgres_microservice_ec2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
