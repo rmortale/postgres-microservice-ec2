848a6d0d89ce232de63ee24d8365b465b4515e84
Antonio Dolce <dolce.antonio@gmail.com>
2024-08-05 12:53:34 +0200

    added rest endpoint

--- src/main/java/ch/dulce/postgres_microservice_ec2/repository/CustomerRepository.java
+++ src/main/java/ch/dulce/postgres_microservice_ec2/repository/CustomerRepository.java
@@ -1,8 +1,14 @@
 package ch.dulce.postgres_microservice_ec2.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@Repository
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
+import java.util.List;
+
+@RepositoryRestResource(collectionResourceRel = "customers", path = "customers")
+public interface CustomerRepository extends PagingAndSortingRepository<Customer, Long>, CrudRepository<Customer, Long> {
+
+    List<Customer> findByLastName(@Param("lastName") String lastName);
 }
